b663c42dcfdbfb8a4cf6c70b7c2f9d41119e4105
uzbfalcon <lochinbekkhujamov1217@gmail.com>
2022-12-22 09:50:44 +0500

    review

---
+++ review.jsx
@@ -0,0 +1,16 @@
+// 1^st Goal
+// Project yasash
+// Projectni strecturalash 
+// RRD get configuration 
+// Create Generic NAvbar
+// To the NEtlify deploying
+
+
+// 2^nd goal
+// Netlify auto deploy via github branch
+// Generic button and input
+// forwardRef()
+
+
+// 3^rd goal
+// Carousel
