3d2259638c81fff609f1f426e5ff19c1392ef106
uzbfalcon <lochinbekkhujamov1217@gmail.com>
2023-02-04 12:50:32 +0500

    review file updated

--- review.jsx
+++ review.jsx
@@ -1,13 +1,15 @@
 // 1-Goal )
-// ➤ Project Yasash
+// ➤ Project Create
 // ➤ Projectni strukturalash (pattern)
 // ➤ react-router-dom@6 ni konfiguratsiya qilish
 // ➤ Generic Navbar yasash
 // ➤ useID()
-// ➤ Netlify ga deploy qilishni o'rganasiz
+
+// ========== COMPLATED
+
+
 
 // 2-Goal )
-// ➤ Netlify auto deploy via github branch
 // ➤ Generic components yasash -> Button, Input
 // ➤ Signin Singup pages
 // ➤ Hidden page larni routeda konfiguratsiya qilish
@@ -15,19 +17,33 @@
 // ➤ Antd orqali dropdown menu yasash
 // ➤ forwardRef()
 
+// ========== COMPLATED
+
+
+
+
 // 3-Goal )
 // ➤ Carousel
 // ➤ Generic House Card
 
+// ========== COMPLATED
+
+
+
+
 // 4-Goal )
 // ➤ Netlify, redirect bug fixed
-// ➤ Swagger haqida tushuncha
 // ➤ Swagger vs Postman
 // ➤ .env file
 // ➤ costum hooks - useReplace() , useSearch()
 // ➤ new URL()
 // ➤ new URLsearchParams()
 // ➤ Filter Globally
+
+// ========== COMPLATED
+
+
+
 
 // 5-Goal )
 // ➤ BUG FIX
@@ -37,11 +53,21 @@
 // ➤ place to center
 // ➤ Change flex to grid
 
+// ========== COMPLATED
+
+
+
+
 // 6-Goal )
 // ➤  Filter completed
 // ➤  Create Select for category
 // ➤  Antd debugging
 // ➤  Properties page completed
+
+// ========== COMPLATED
+
+
+
 
 // 7-Goal )
 // ➤ Recomanded
@@ -51,7 +77,49 @@
 // ➤ Footer
 // ➤ Selecting single item
 
+// ========== COMPLATED
+
+
+
+
 // 8-Goal )
 // ➤ Signin
 // ➤ Add feature to generic button and input
 // ➤ update useRequst custom hooks
+
+// ========== COMPLATED
+
+
+
+
+// 9-Goal )
+// ➤ Signin bug fixed
+// ➤ All other bugs reviewed
+// ➤ Properties page finished
+// ➤ Token saqalashdagi hatoliklar
+// ➤ Single Itemode  page
+
+// ========== COMPLATED
+
+
+
+
+// 10-Goal )
+// ➤ details page finished
+// ➤ react-yandex-maps
+
+// ========== COMPLATED
+
+
+
+
+// 11-Goal )
+// ➤ GitHub Project section intro
+// ➤ Favourite page completed
+// ➤ Like and Dislike implemented
+// ➤ React-Query
+// ➤ useQuery(dep, fetch, response)
+// ➤ contextAPI implemented
+// ➤ Navbar register button fixed
+// ➤ Register Tabs => signin, signup
+// ➤ SignUp
