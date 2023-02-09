061a978108e99ccda61444e34149069c1293990e
uzbfalcon <lochinbekkhujamov1217@gmail.com>
2023-02-09 11:38:26 +0500

    bugs fixed

--- src/components/Banner/index.jsx
+++ src/components/Banner/index.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
-import {  Blur, Container, Content, Img } from './style';
-import {Button} from '../Generic'
+import { Blur, Container, Content, Img } from './style';
+import { Button } from '../Generic'
 
 import img1 from '../../assets/img/photo1.jpg';
 
@@ -10,15 +10,15 @@ export const GetCarousel = () => {
 
   return (
     <Container>
-        <Img src={img1} />
- 
+      <Img src={img1} />
+
       <Blur />
       <Content>
         <Content.Title>Vermont Farmhouse With Antique Jail Is the Week's Most Popular Home</Content.Title>
-        
+
         <Button>Read More</Button>
       </Content>
-      </Container>
+    </Container>
   );
 };
 
