c33500a0289bc55f41a541fbfaf5135a7ee05b19
uzbfalcon <lochinbekkhujamov1217@gmail.com>
2023-06-04 22:43:32 +0500

    some bugs fixed

--- src/components/Banner/index.jsx
+++ src/components/Banner/index.jsx
@@ -1,21 +1,16 @@
 import React from 'react';
 import { Blur, Container, Content, Img } from './style';
 import { Button } from '../Generic'
-
 import img1 from '../../assets/img/photo1.jpg';
 
 export const GetCarousel = () => {
 
-
-
   return (
     <Container>
       <Img src={img1} />
-
       <Blur />
       <Content>
         <Content.Title>Vermont Farmhouse With Antique Jail Is the Week's Most Popular Home</Content.Title>
-
         <Button>Read More</Button>
       </Content>
     </Container>
